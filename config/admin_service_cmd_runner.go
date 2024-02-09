b735756f623f12fe5d5c4fa55f83bc8785291058
Hiddify <hiddify@gmail.com>
2024-02-09 23:36:44 +0100

    fix: bug

--- config/admin_service_cmd_runner.go
+++ config/admin_service_cmd_runner.go
@@ -10,12 +10,12 @@ import (
 )
 
 func ExecuteCmd(executablePath, args string) (string, error) {
-	cwd:=filepath.Dir(executablePath)
-	background=false
-	if appimage:=os.Getenv("APPIMAGE"); appimage!=""{
-		executablePath= appimage+" HiddifyService"
-		args=""
-		background=true
+	cwd := filepath.Dir(executablePath)
+	background := false
+	if appimage := os.Getenv("APPIMAGE"); appimage != "" {
+		executablePath = appimage + " HiddifyService"
+		args = ""
+		background = true
 	}
 	if err := execCmdImp([]string{"gksu", executablePath, args}, cwd, background); err == nil {
 		return "Ok", nil
@@ -23,31 +23,30 @@ func ExecuteCmd(executablePath, args string) (string, error) {
 	if err := execCmdImp([]string{"pkexec", executablePath, args}, cwd, background); err == nil {
 		return "Ok", nil
 	}
-	if err := execCmdImp([]string{"xterm", "-e", "sudo " + executablePath + " " + args }, cwd, background); err == nil {
+	if err := execCmdImp([]string{"xterm", "-e", "sudo " + executablePath + " " + args}, cwd, background); err == nil {
 		return "Ok", nil
 	}
-	
-	if err := execCmdImp([]string{"sudo",  executablePath, args }, cwd, background); err == nil {
+
+	if err := execCmdImp([]string{"sudo", executablePath, args}, cwd, background); err == nil {
 		return "Ok", nil
 	}
-	
-	
+
 	return "", fmt.Errorf("Error executing run as root shell command")
 
 }
 
-func execCmdImp(commands []string,cwd string, background bool) error {
+func execCmdImp(commands []string, cwd string, background bool) error {
 	cmd := exec.Command(commands[0], commands[1:]...)
 	cmd.Dir = cwd
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	fmt.Printf("Running command: %v", commands)
-	if background{
+	if background {
 		if err := cmd.Start(); err != nil {
 			fmt.Printf("Error: %v\n", err)
 			return err
 		}
-	}else{
+	} else {
 		if err := cmd.Run(); err != nil {
 			fmt.Printf("Error: %v\n", err)
 			return err
