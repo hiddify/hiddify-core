59feeba6b7a45aa0bcaaab76e355bab38241adc1
Hiddify <hiddify@gmail.com>
2024-02-24 20:28:24 +0100

    disable windows service due to False Positive in Anti virus

--- config/admin_service_cmd_runner_windows.go
+++ config/admin_service_cmd_runner_windows.go
@@ -2,27 +2,22 @@
 
 package config
 
-import (
-	"os"
-	"syscall"
-
-	"golang.org/x/sys/windows"
-)
+// "golang.org/x/sys/windows"
 
 func ExecuteCmd(exe string, args string, background bool) (string, error) {
-	verb := "runas"
-	cwd, _ := os.Getwd()
+	// verb := "runas"
+	// cwd, _ := os.Getwd()
 
-	verbPtr, _ := syscall.UTF16PtrFromString(verb)
-	exePtr, _ := syscall.UTF16PtrFromString(exe)
-	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
-	argPtr, _ := syscall.UTF16PtrFromString(args)
+	// verbPtr, _ := syscall.UTF16PtrFromString(verb)
+	// exePtr, _ := syscall.UTF16PtrFromString(exe)
+	// cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+	// argPtr, _ := syscall.UTF16PtrFromString(args)
 
-	var showCmd int32 = 1 //SW_NORMAL
+	// var showCmd int32 = 1 //SW_NORMAL
 
-	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
-	if err != nil {
-		return "", err
-	}
+	// err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+	// if err != nil {
+	// 	return "", err
+	// }
 	return "", nil
 }
