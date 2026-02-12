f5b584be38d492109893b9d758ad747bb5520ce4
hiddify <114227601+hiddify-com@users.noreply.github.com>
2026-02-12 13:52:01 +0330

    fix: workflow 1

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,4 @@
 go mod tidy
-TAGS=with_gvisor,with_quic,with_wireguard,with_utls,with_clash_api,with_grpc
+TAGS=with_v2ray_api,with_gvisor,with_quic,with_dhcp,with_wireguard,with_utls,with_acme,with_clash_api,with_tailscale,with_ccm,with_ocm,tfogo_checklinkname0
 # TAGS=with_dhcp,with_low_memory,with_conntrack
 go run --tags $TAGS ./cmd/main  $@
